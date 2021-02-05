32a1058f6a9eb22ffc4255766a244b173b6104cf
mayjunjuly0313 <tank6138@gmail.com>
2021-02-05 14:45:02 +0900

    refactor: bug fix

--- model/db.js
+++ model/db.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 const config = require('../config').mysqlOptions;
-const Database = require('./Database');
+// const Database = require('./Database');
 
 // const db = new Database(config.mysqlOptions);
 const db = mysql.createConnection(config);
