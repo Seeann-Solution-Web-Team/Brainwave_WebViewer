ab349398b920fbd13d2a669d3c063cf8b0da665c
mayjunjuly0313 <tank6138@gmail.com>
2021-02-05 14:26:07 +0900

    refactor: Add config file

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mysqlOptions: {
+    host: 'localhost',
+    user: 'seeann_mysql',
+    password: '@Hse05040!',
+    database: 'seeann_web',
+    multipleStatements: false,
+  },
+};
